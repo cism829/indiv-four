d6621512bc0bf21b2092932b8bc41bbc7efcab18
cism829 <clrodgers@uncg.edu>
2024-05-28 17:52:00 -0400

    new files

---
+++ src/main/java/csc340/example/indiv_four/IndivFourApplication.java
@@ -0,0 +1,13 @@
+package csc340.example.indiv_four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndivFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndivFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csc340/example/indiv_four/IndivFourApplicationTests.java
@@ -0,0 +1,13 @@
+package csc340.example.indiv_four;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndivFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
